cb5d2b0c0c0d75633f6feae665208cfe43034135
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-11-04 08:30:05 -0800

    Start defining entry point

---
+++ Sandbox/src/Sandbox.cpp
@@ -0,0 +1,24 @@
+#include <Hazel.h>
+
+class Sandbox : public Hazel::Application
+{
+public:
+	Sandbox()
+	{
+
+	}
+
+	~Sandbox()
+	{
+
+	}
+
+};
+
+int main()
+{
+	Sandbox* sandbox = new Sandbox();
+	sandbox->Run();
+
+	delete sandbox; // release resources
+}
