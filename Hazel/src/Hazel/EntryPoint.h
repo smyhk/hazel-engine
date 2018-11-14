fd36d545ed794935f96522c7194d129adf6d317e
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-11-14 10:46:24 -0800

    Implement core and client logging

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -6,6 +6,10 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Hazel::Log::Init();
+	Hazel::Log::GetCoreLogger()->warn("Initialized Core Log!");
+	Hazel::Log::GetClientLogger()->info("Hello Client!");
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
